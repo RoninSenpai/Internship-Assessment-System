59b928d52be01943e35734c9dd54f415348c66a1
CIRNO <juliusantonaquino.joules@gmail.com>
2025-03-29 20:20:19 +0800

    Sidebar finished, added to all templates

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
+
 function toggleSidebar() {
-    document.querySelector(".sidebar").classList.toggle("open");
+    console.log("Sidebar toggled");
+    window.parent.document.querySelector(".sidebar").classList.toggle("open");
+    document.querySelectorAll(".sidebar-item").forEach(item => {
+        item.classList.toggle("open");
+    });
+}
+
+function changeIframe(newSrc) {
+    window.parent.document.getElementById("content").src = newSrc;
 }
